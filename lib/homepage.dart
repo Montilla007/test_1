262a6a19d687a89560a5ee618caefa04b8c82a52
Christer Dale Montilla Reyes <rchristerdale@gmail.com>
2024-08-10 11:57:11 +0800

    Update homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,23 +1,4 @@
-// import 'package:flutter/material.dart';
 
-// class HomePage extends StatefulWidget {
-//   const HomePage({super.key});
-
-//   @override
-//   State<HomePage> createState() {
-//     return _HomePageState();
-//   }
-//   // _HomePageState createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Scaffold(
-
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
